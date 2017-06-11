56c87084a5264389c0147b3f755357216c6696e7
mattdesl <dave.des@gmail.com>
2017-06-11 12:59:55 -0400

    random isometric tests

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('babel-polyfill');
 require('fastclick')(document.body);
 
 const MainScene = require('./lib/components/MainScene');
+const createBackground = require('three-vignette-background');
 
 const { assets, renderer } = require('./lib/context');
 const query = require('./lib/util/query');
+const { randomFloat, shuffle } = require('./lib/util/random');
+const palettes = require('nice-color-palettes/500');
+
 const isMobile = require('./lib/util/isMobile');
 const createApp = require('./lib/createApp');
 const createLoop = require('raf-loop');
@@ -28,14 +32,25 @@ function start () {
     alpha: true
   });
 
+  const colors = [ 'white', 'black' ]
+  // const colors = shuffle(palettes[Math.floor(randomFloat(palettes.length))]).slice(0, 2);
+  // const background = colors[0];
+  const background = colors.shift();
   // const background = '#f9f9f9';
-  const background = '#fff';
+
   document.body.style.background = background;
   app.renderer.setClearColor(background, 1);
   app.renderer.gammaOutput = false;
+  app.scene.fog = new THREE.FogExp2('white', 0.1);
 
   const components = [];
-  addComponent(new MainScene(app));
+  addComponent(createBackground({
+    noiseAlpha: 0.0,
+    // grainScale: 1 / window.innerWidth,
+    scale: new THREE.Vector2(1, 1).multiplyScalar(3),
+    colors: [ background, new THREE.Color(background).offsetHSL(0, 0.25, -0.25).getStyle() ]
+  }));
+  addComponent(new MainScene(app, colors));
   document.body.classList.remove('hide-cursor');
   document.body.classList.add('grab');
   canvas.style.visibility = '';
